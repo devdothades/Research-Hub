8dc0c3c4e038c1c77cbdf97c38354d33582dbf09
Jai <141633312+devdothades@users.noreply.github.com>
2024-08-12 20:39:14 +0800

    Update db.php

--- db.php
+++ db.php
@@ -3,7 +3,7 @@
 //change the values of these variables according to your local machine
 $hostname = 'localhost';
 $username = 'root';
-$password = 'hm0ejd74';
+$password = '';
 $database_name = 'ACLC';
 
 $conn = mysqli_connect($hostname,$username,$password,$database_name);
